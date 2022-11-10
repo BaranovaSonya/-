d0878e434fb39756dfe7b273113457e291350b4a
BaranovaSonya <114420047+BaranovaSonya@users.noreply.github.com>
2022-11-10 07:59:30 +0300

    Add files via upload

---
+++ решатель задач Баранова.py
@@ -0,0 +1,34 @@
+print("Какую задачу нужно решить: a,b,c")
+a = "a) кодирование информации"
+b = "b) кодирование звука"
+c = "c) кодирование изображения"
+print(a, b, c)
+bukva=input("Выберите вид задачи")
+print("вид задачи")
+if bukva == a:
+    print("выбор неизвестного: I,N,i,K")
+    neizvestnoe = input()
+    if neizvestnoe == N:
+        i=(input)
+        print(N = 2^i)
+    elif neizvestnoe == I:
+        i =(input)
+        K =(input) 
+        print(I = K*i)
+    elif neizvestnoe == i:
+        I =(input)
+        K =(input)
+        print(i = I\K)
+    elif neizvestnoe == K:
+        I =(input)
+        i =(input)
+        print(K = I\i)
+                   
+        
+        if bukva == b:
+        print("выбор неизвестного: I,v,i,Ch,t")
+        
+        if bukva == c:
+        print("выбор неизвестного: 
+            
+
