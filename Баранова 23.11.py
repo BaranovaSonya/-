c19aa70b7cfae274c4ab720b90eac71bd6f7bb4f
BaranovaSonya <114420047+BaranovaSonya@users.noreply.github.com>
2022-11-24 07:57:55 +0300

    Update Баранова 23.11.py

--- Баранова 23.11.py
+++ Баранова 23.11.py
@@ -1,11 +1,16 @@
-from PIL import Image, ImageDraw, ImageFont
-im = Image.new('RGB',(200,200),color=('#336699'))
+a=int(3)
+b=int(1)
+spisok=['НА СТАРТ','ВНИМАНИЕ','ОБЕД!!!']
+for i in range:
+    from PIL import Image, ImageDraw, ImageFont
+    im = Image.new('RGB',(200,200),color=('#336699'))
+    font = ImageFont.truetype('C:/Windows/Fonts/Book Antiqua/BKANT.TTF', size=22)
+    draw_text = ImageDraw.Draw(im)
+    draw_text.text(
+        (100,100),
+        '1',
+        font=font,
+        fill=('#1C0606')
+        )
 im.show()
-font = ImageFont.truetype('C:\Windows\Fonts\Book Antiqua', size=22)
-draw_text = ImageDraw.Draw(im)
-draw_text.text(
-    (100, 100),
-    'Conan Gray',
-    font=font,
-    fill='#1C0606')
-im.show()
+
