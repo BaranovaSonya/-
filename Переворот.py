dabe28ac2f25257d4388cf0be0513e8ce5cc246e
BaranovaSonya <114420047+BaranovaSonya@users.noreply.github.com>
2022-12-14 12:00:10 +0700

    Add files via upload

---
+++ Переворот.py
@@ -0,0 +1,17 @@
+def f(a):
+    print(a[: :-1])
+    
+def k(b):
+    return(a[: :-1])
+    return(c)
+
+a = input('фамилия')
+c = input('пол')
+f(a)
+t = k(a)
+print(c)
+
+g = ['а','о','у','я','е','ё','ю','и','э','ы']
+for i in g:
+    if:
+        print()
