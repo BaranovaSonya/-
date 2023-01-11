e7267a6aa53d3765a40a1d6daeb0085919c7b7e1
BaranovaSonya <114420047+BaranovaSonya@users.noreply.github.com>
2023-01-11 13:39:50 +0700

    Delete таблица умножения .py

--- таблица умножения .py
+++
@@ -1,9 +0,0 @@
-p=int()
-p=int(input(f"{p}-ичная таблица умножения"))
-for x in range(1,p):
-    for y in range(1,p):
-        z=(x*y//p)*10+(x*y)%p
-        print(z,end=' ')
-    print()
-        
-        
