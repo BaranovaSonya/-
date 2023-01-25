d4889d102dd8aa803b7b6e8a0d4a39fdb0a29c19
BaranovaSonya <114420047+BaranovaSonya@users.noreply.github.com>
2023-01-25 11:49:29 +0700

    Update решения.py

--- 2 Полугодие/решения.py
+++ 2 Полугодие/решения.py
@@ -66,9 +66,12 @@ def f14():
             
             
 def f15():   
+    sp = []
+    if all(sp):
+        print ('есть')
+
     for a in range (1,1000):
         if all (((x%2==0) <= (x%3!=0)) or (x+a>=100) for x in range(1,1000)):
                    print(a)
                    break
-
 
